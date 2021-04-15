bfe2eb39ff61133648d9d302ea5f76271aa24a72
Linh <manhlinh1258@gmail.com>
2021-04-15 21:01:21 +0700

    add

--- index.js
+++ index.js
@@ -5,8 +5,6 @@
  import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 import App from './App';
-import Home from './Home';
-import Product from './Product';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
